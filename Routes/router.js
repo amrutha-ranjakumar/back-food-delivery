0531373c41bbb726bed16fe8715f3799b1e8ad32
amrutha-ranjakumar <147486960+amrutha-ranjakumar@users.noreply.github.com>
2024-11-11 18:27:59 +0530

    food delivery using node

--- Routes/router.js
+++ Routes/router.js
@@ -3,9 +3,6 @@ const userController = require('../Controllers/userController');
 const dishesController = require('../Controllers/dishesController');
 const jwtMiddleware = require('../Middlewares/jwtMiddleware');
 const multerConfig = require('../Middlewares/multerMiddleware');
-const menuController = require('../Controllers/menuController');
-
-
 const deliveryInformationController = require('../Controllers/deliveryInformationController');
 
 const router = new express.Router();
@@ -16,10 +13,7 @@ router.post('/user/login', userController.login);
 
 // Dishes and menu routes
 router.post('/dishesmenu/add', jwtMiddleware, multerConfig.fields([{ name: 'menuimage' }, { name: 'dishesImage' }]), dishesController.adddishesmenu);
-router.post('/menu/add', jwtMiddleware, multerConfig.single('menuimage'), menuController.menu);
 router.get('/dishesmenu/all-dishesmenu', jwtMiddleware, dishesController.getAlldishes);
-router.get('/menus/all-menus', jwtMiddleware, menuController.getAllmenus);
-
 
 // Route to add delivery information
 router.post('/addDeliveryInformation', deliveryInformationController.addDeliveryInformation);
